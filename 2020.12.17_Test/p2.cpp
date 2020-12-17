d75f529f6d61d9fae5414cf5e684f6e5d70f3e38
Pozdeev417-2 <76137772+Pozdeev417-2@users.noreply.github.com>
2020-12-17 06:35:48 +0300

    Add files via upload

---
+++ 2020.12.17_Test/p2.cpp
@@ -0,0 +1,13 @@
+#include<locale.h> // заголовочгый файл для функции setlocale()
+#include<iomanip> // заголовочный файл для cout, « и много чего еще
+#include<bitset>
+#include<iostream>
+#include<math.h> // библиотека для округления чисел
+#include <cstdio>
+#include <fstream>
+int main()
+{
+    system("pause");
+    return 0;
+
+}
